2effd54c71aff4a97a8224703c06839548862ef3
Vinnie Falco <vinnie.falco@gmail.com>
2017-03-13 13:07:18 -0400

    [wip]

--- test/http/design.cpp
+++ test/http/design.cpp
@@ -9,6 +9,9 @@
 
 #include <beast/core/error.hpp>
 #include <beast/http/basic_parser.hpp>
+#include <beast/http/concepts.hpp>
+#include <beast/http/header_parser.hpp>
+#include <beast/http/message_parser.hpp>
 
 namespace beast {
 namespace http {
@@ -27,33 +30,31 @@ Parse states:
 
 */
 
-#if 0
 template<
     class SyncReadStream,
     class DynamicBuffer,
-    class Parser>
+    bool isRequest,
+    class Fields>
 void
-new_parse(
+parse_some(
     SyncReadStream& stream,
     DynamicBuffer& dynabuf,
-    Parser& parser,
+    header_parser<isRequest, Fields>& parser,
     error_code& ec)
 {
     static_assert(is_SyncReadStream<SyncReadStream>::value,
         "SyncReadStream requirements not met");
     static_assert(is_DynamicBuffer<DynamicBuffer>::value,
         "DynamicBuffer requirements not met");
-    static_assert(is_Parser<Parser>::value,
-        "Parser requirements not met");
-    for(;;)
+    BOOST_ASSERT(parser.need_more());
+    BOOST_ASSERT(! parser.is_done());
+    auto used =
+        parser.write(dynabuf.data(), ec);
+    if(ec)
+        return;
+    dynabuf.consume(used);
+    if(parser.need_more())
     {
-        auto used =
-            parser.write(dynabuf.data(), ec);
-        if(ec)
-            return;
-        dynabuf.consume(used);
-        if(! parser.need_more())
-            break;
         boost::optional<typename
             DynamicBuffer::mutable_buffers_type> mb;
         auto const size =
@@ -77,45 +78,123 @@ new_parse(
             if(ec)
                 return;
             BOOST_ASSERT(! parser.need_more());
-            break;
         }
-        if(ec)
+        else if(ec)
+        {
             return;
+        }
     }
 }
 
+/** Parse some data from the stream.
+*/
 template<
     class SyncReadStream,
     class DynamicBuffer,
     bool isRequest,
-    class Derived>
+    class Body,
+    class Fields>
 void
-parse_body_direct(
+parse_some(
     SyncReadStream& stream,
     DynamicBuffer& dynabuf,
-    basic_parser<isRequest, Derived>& parser,
+    message_parser<isRequest, Body, Fields>& parser,
     error_code& ec)
 {
-    switch(parser.body_style())
+    static_assert(is_SyncReadStream<SyncReadStream>::value,
+        "SyncReadStream requirements not met");
+    static_assert(is_DynamicBuffer<DynamicBuffer>::value,
+        "DynamicBuffer requirements not met");
+    BOOST_ASSERT(! parser.is_done());
+    // See if the parser needs more structured
+    // data in order to make forward progress
+    //
+    if(parser.need_more())
     {
-    case 0: // content-length
-    {
-
-        break;
+        // Give the parser what we have already
+        //
+        auto used =
+            parser.write(dynabuf.data(), ec);
+        if(ec)
+            return;
+        dynabuf.consume(used);
+        if(parser.need_more())
+        {
+            // Parser needs even more, try to read it
+            //
+            boost::optional<typename
+                DynamicBuffer::mutable_buffers_type> mb;
+            auto const size =
+                read_size_helper(dynabuf, 65536); // magic number?
+            BOOST_ASSERT(size > 0);
+            try
+            {
+                mb.emplace(dynabuf.prepare(size));
+            }
+            catch(std::length_error const&)
+            {
+                // Convert the exception to an error
+                ec = error::buffer_overflow;
+                return;
+            }
+            auto const bytes_transferred =
+                stream.read_some(*mb, ec);
+            if(ec == boost::asio::error::eof)
+            {
+                dynabuf.commit(bytes_transferred);
+                // Caller will see eof on next read.
+                ec = {};
+                parser.write_eof(ec);
+                if(ec)
+                    return;
+                BOOST_ASSERT(! parser.need_more());
+                BOOST_ASSERT(parser.is_done());
+            }
+            else if(! ec)
+            {
+                dynabuf.commit(bytes_transferred);
+            }
+            else
+            {
+                return;
+            }
+        }
     }
-
-    case 1: // eof
+    else if(! parser.is_done())
     {
-        break;
-    }
+        // Apply any remaining bytes in dynabuf
+        //
+        parser.consume(dynabuf, ec);
+        if(ec)
+            return;
 
-    case 2: // chunked
-    {
-        break;
-    }
+        // Parser wants a direct read
+        //
+        auto const mb = parser.prepare(
+            dynabuf, 65536); // magic number?
+        auto const bytes_transferred =
+            stream.read_some(mb, ec);
+        if(ec == boost::asio::error::eof)
+        {
+            dynabuf.commit(bytes_transferred);
+            // Caller will see eof on next read.
+            ec = {};
+            parser.write_eof(ec);
+            if(ec)
+                return;
+            BOOST_ASSERT(! parser.need_more());
+            BOOST_ASSERT(parser.is_done());
+        }
+        else if(! ec)
+        {
+            parser.commit(bytes_transferred);
+        }
+        else
+        {
+            return;
+        }
     }
 }
-#endif
 
 class design_test : public beast::unit_test::suite
 {
